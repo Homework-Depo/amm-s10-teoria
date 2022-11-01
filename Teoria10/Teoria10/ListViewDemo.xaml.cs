d7e80d45a1c4721bb40ea0ef8104a1c496a7bea6
javier-aponte <31341776+javier-aponte@users.noreply.github.com>
2022-11-01 13:34:10 -0500

    Add project files.

---
+++ Teoria10/Teoria10/ListViewDemo.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Teoria10
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewDemo : ContentPage
+    {
+        public ListViewDemo()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Teoria10/Teoria10/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Teoria10
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btn1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ViewToViewDemo());
+            };
+            btn2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new BindingModeDemo());
+            };
+            btn3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ListViewDemo());
+            };
+            btn4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new PickerDemo());
+            };
+        }
+    }
+}
